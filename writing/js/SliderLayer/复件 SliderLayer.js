e246b0f890fc8c955413a1ad700ecac2504b1a7e
louxiaojian <louxiaojian@55d9cdf4-3184-11de-a1c6-114bfcb2beae>
2010-06-24 06:00:50 +0000

    1.SliderLayer增加功能

--- writing/js/SliderLayer/复件 SliderLayer.js
+++ writing/js/SliderLayer/复件 SliderLayer.js
@@ -31,14 +31,13 @@ function SliderLayer(s){
 	 if(!this.options.handle || !this.options.handleBx || this.options.handle.length!=this.options.handleBx.length) return false;
 	 this.handle=this.options.handle;
 	 this.handleBx=this.options.handleBx;
-	 this.y=this.interval=this.options.interval;
+	 this.interval=this.options.interval;
 	 this.delay=this.options.delay;
 	 this.Tween=this.options.Tween;
 	 this.evt=this.options.evt;
-	 this.cl=this.options.cl;
-	 this.index=this.options.index;
 	 this.start=this.B(this,this.options.start);
 	 this.end=this.B(this,this.options.end);
+	 this.rKey=true;//控制是否执行展开函数
 	 this.run();
 }
 SliderLayer.prototype={
@@ -106,15 +105,11 @@ SliderLayer.prototype={
 	  },
 	 SetOptions:function(s){
 		this.options={
-			handle:[],
-			handleBx:[],
+			handle:function(){},
+			handleBx:function(){},
 			evt:'click',//触发菜单的方式
-			eKey:0,//0为收缩状态，1为展开状态
 			interval:40,//下拉缓动的时间
 			delay:10,//下拉延时时间
-			cl:0,//关闭上一个展开的(1:要关闭；0：不关闭)
-			cs:1,//关闭自己(1:要关闭；0：不关闭)
-			index:'',
 			dis:['height'],
 	        Tween:function(t,b,c,d){return c*(t/=d)*t + b;},
 			start:function(){},
@@ -123,46 +118,44 @@ SliderLayer.prototype={
 		  this.E(this.options,s||{})
 	  },
 	 slider:function(o,hb){
-			 var b=0,c=this.getEle(hb).h,d=this.interval,t=0;
-			 if(!hb.style.overflow) hb.style.overflow='hidden';
-			 if(o.timer) clearTimeout(o.timer);
-		     this.y=0;
-			 function sMove(){
-				 this.setStyle(hb,{'height': (o.mKey ? (Math.ceil(this.Tween(t,b,c,d)) + "px") : (c-Math.ceil(this.Tween(t,b,c,d)) + "px"))})
-				 if(t<d){
-					 t++;o.timer=setTimeout(this.B(this,sMove), 10);
-				 }else{
-				   this.y=t;
-				   if(!o.mKey) this.setStyle(hb,{'display':'none'});
-				   this.setStyle(hb,{'height':'auto'});
-				   t=0;
-				   o.mKey=o.mKey ? 0 : 1
-				   this.end();
-				   if(this.lm!=hb && this.ln!=o) this.lm=hb;this.ln=o
-				 };
-			 };
+		 var n=this.index=o.nub,hb=this.handleBx[n],b=0,c=this.getEle(hb).h,d=this.interval,t=0,pt=cssValue(hb,'padding-top'),pb=cssValue(hb,'padding-bottom'),bt=cssValue(hb,'border-top'),bb=cssValue(hb,'border-bottom');
+		 if(!hb.style.overflow) hb.style.overflow='hidden';
+		 if(this.timer) clearTimeout(this.timer);
+		 
+		 this.sMove=function(){
+			 if(o.mKey){
+			    /*展开*/
+				if(bt!='0px'){this.setStyle(hb,{'border-top':(Math.ceil(this.Tween(t,b,bt,d)) + "px")})}
+			    if(bt!='0px'){this.setStyle(hb,{'height':(Math.ceil(this.Tween(t,b,c,d)) + "px")})}
+			    }else{
+			    /*收起*/
+			    this.setStyle(hb,{'height':(c-Math.ceil(this.Tween(t,b,c,d)) + "px")})
+			  }
+
 			 
-			 sMove.call(this);
+			 if(t<d){
+				 t++;this.timer=setTimeout(this.B(this,this.sMove), 10);
+			 }else{
+			   if(!o.mKey) this.setStyle(hb,{'display':'none'});
+			   this.setStyle(hb,{'height':'auto'});
+			   this.rKey=true;
+			   t=0;
+			   this.end();
+			 };
+		 };
+		 this.sMove();
 		 
 	 },
 	 act:function(o){
-			if(this.cl && this.ln==o) return false;//要关闭上一个展开的情况下，再点击自己不收起或展开
-			if(this.y!=this.interval) return false;//一个正在展开或收起时不展开另外一个.
+			if(!this.rKey){return false}
 			var n=this.index=o.nub,hb=this.handleBx[n];
 			if(this.sTime) clearTimeout(this.sTime);
 			this.sTime=setTimeout(this.B(this,function(){
-				  o.mKey=(cssValue(hb,'display')=='none' ||  hb.offsetHeight==0 ) ? 1 : 0;
-				  if(cssValue(hb,'display')=='none' || cssValue(hb,'visibility')=='hidden'){hb.style.display='block';hb.style.visibility='visible'};
-				  this.start();	
-				  if(this.cl){
-					  if(!this.lm && !this.ln){
-						  this.lm=hb;this.ln=o
-						  }else{
-						  if(!this.ln.mKey && this.ln!=o) {this.slider(this.ln,this.lm)}
-					  }
-				  };
-				  this.slider(o,hb);
-				  
+				  this.rKey=false;
+				  o.mKey=(hb.style.display=='none' ||  hb.offsetHeight==0 ) ? 1 : 0;
+				  if(hb.style.display && hb.style.display=='none'){hb.style.display='block'};
+				  this.start();									   
+				  this.slider(o,hb);									   
 			}),this.delay) 
 	 },
 	 
@@ -172,7 +165,6 @@ SliderLayer.prototype={
 			  this.handle[i].nub=i;
 			  this.on(this.handle[i],this.evt,this.B(this,this.act,this.handle[i]));
 		  }
-		  if(this.index>=0 && this.cl) {this.act(this.handle[this.index]);}
 	 }
 	  
 };
@@ -190,44 +182,18 @@ function getCN(node, name, type) {
 	  }
 };
 domReady(function(){
-	  var d=new Date().getTime();
 	  var tvguide=function(){
-			var h=getCN($('exp1'),'SliderHandle','div'),hb=getCN($('exp1'),'SliderBx','div');
+			var h=getCN(document,'SliderHandle','div'),hb=getCN(document,'SliderBx','div');
 			new SliderLayer({
 							'handle':h,
 							'handleBx':hb,
 							'end':function(){},
 							'start':function(){this.handle[this.index].innerHTML=this.handle[this.index].mKey ? '收缩' : '展开'},
 							'evt':'click',
-							'cl':0,
 							'Tween':function(t,b,c,d){
 								if ((t/=d/2) < 1) return c/2*t*t + b;
 								return -c/2 * ((--t)*(t-2) - 1) + b;
 							}
 			})
 	  }();
-	  
-	  var tvguide2=function(){
-			var h=getCN($('exp2'),'SliderHandle','div'),hb=getCN($('exp2'),'SliderBx','div');
-			new SliderLayer({
-							'handle':h,
-							'handleBx':hb,
-							'end':function(){},
-							'start':function(){
-								if(this.ln) this.ln.innerHTML=this.ln.mKey ? '收缩' : '展开';
-								this.handle[this.index].innerHTML=this.handle[this.index].mKey ? '收缩' : '展开'
-							 },
-							'evt':'mouseover',
-							'cl':1,
-							'index':0,
-							'interval':40,
-							'delay':50,
-							'Tween':function(t,b,c,d){
-								if ((t/=d/2) < 1) return c/2*t*t + b;
-								return -c/2 * ((--t)*(t-2) - 1) + b;
-							}
-			})
-	  }();
-	 // alert(new Date().getTime()-d)
-	  
 })
